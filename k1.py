c160309a7bfaa5aa192c10672acc8961c7f939b7
kuldeep <dkuldeep11@gmail.com>
2016-01-29 23:26:47 -0800

    Adding the small script of execute and sleep any random process

---
+++ k1.py
@@ -0,0 +1,4 @@
+print "Hello world"
+print '''this is all the newline string to be testted
+to write to console.
+Hope it works'''
---
+++ process_with_time_gap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in 1 2 3 4 5
+do
+   echo "Welcome $i times"
+   python k1.py >> op1
+   sleep 2
+done
